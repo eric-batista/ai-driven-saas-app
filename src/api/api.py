472586188d4018ead9356cec37641957c4a239f9
Eric Batista <klose.eric31@gmail.com>
2022-04-05 20:24:44 -0300

    feat: implement the route /generate_snippet_and_keywords and validate the prompt input in API

--- src/api/api.py
+++ src/api/api.py
@@ -7,11 +7,25 @@ import sys
 from os.path import abspath
 sys.path.append(abspath('./../../'))
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from src.app.ai_model import generate_branding_snippet, generate_keywords
 
 
 app = FastAPI()
+
+
+MAX_INPUT_LENGTH = 32
+
+
+def validate_input_lenght(prompt: str):
+    '''
+    >>>  This function validates the length of prompt input and raise 400 status code
+    '''
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(
+            status_code=400,
+            detail=f'Input length is too long. Must be under {MAX_INPUT_LENGTH} characters.'
+        )
 
 
 @app.get('/generate_snippet')
@@ -19,16 +33,29 @@ async def generate_branding_snippet_route(prompt: str):
     '''
     >>> This route use the GET method to generate branding snippet
     '''
+    validate_input_lenght(prompt)
     snippet = generate_branding_snippet(prompt)
 
-    return {'message': snippet}
+    return {'sninppet': snippet, 'keywords': []}
 
 
-@app.get('/generate_keyword')
+@app.get('/generate_keywords')
 async def generate_branding_keyword_route(prompt: str):
     '''
     >>> This route use the GET method to generate keywords
     '''
-    snippet = generate_keywords(prompt)
+    validate_input_lenght(prompt)
+    keywords = generate_keywords(prompt)
 
-    return {'keywords': snippet}
+    return {'snippet': None, 'keywords': keywords}
+
+@app.get('/generate_snippet_and_keywords')
+async def generate_branding_keyword_route(prompt: str):
+    '''
+    >>> This route use the GET method to generate branding snippet and keywords
+    '''
+    validate_input_lenght(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+
+    return {'snippet': snippet, 'keywords': keywords}
